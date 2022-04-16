1f033bb00013aa5c7b80266dc9319f299136bcdc
@anhnv99 <vietanhhappy99@gmail.com>
2022-04-16 12:33:21 +0700

    add

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -14,10 +14,6 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs')
 }
 
-// object: {
-//     key: '',
-//     value: ''
-// }
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage
